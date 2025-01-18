3e44e3d4723dc0ab3f8f9b80bbdf7c5bc89eeafa
Daniyel <d_slambek@kbtu.kz>
2025-01-18 23:05:32 +0500

    Assign Multiple Values

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,13 @@
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR = "John"
-myvar2 = "John"
-myVariableName = "John" #Camel Case
-MyVariableName = "John" #Pascal Case
-my_variable_name = "John" #Snake Case
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
