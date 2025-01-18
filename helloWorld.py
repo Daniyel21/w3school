54e6f5385652684e304b1bb302cdf2fab6e8d45c
Daniyel <d_slambek@kbtu.kz>
2025-01-19 00:29:33 +0500

    Escape Characters

--- helloWorld.py
+++ helloWorld.py
@@ -1,11 +1,11 @@
-age = 36
-txt = f"My name is John, I am {age}"
-print(txt)
-price = 59
-txt = f"The price is {price} dollars"
-print(txt)
-price = 59
-txt = f"The price is {price:.2f} dollars"
-print(txt)
-txt = f"The price is {20 * 59} dollars"
-print(txt)
+txt = "We are the so-called \"Vikings\" from the north."
+'''
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value'''
